9fa01a4355c4f49fb9b1452210b5bb32450d9f16
Levi Borges Ferreira <borgesferreiralevi@gmail.com>
2026-05-09 08:56:04 -0400

    Delete Repositories directory

--- Repositories/ILivroRepositories.cs
+++
@@ -1,9 +0,0 @@
-using Biblioteca.Models;
-
-namespace Biblioteca.Repositories;
-
-public interface ILivroRepository
-{
-    Task<List<Livro>> BuscarTodosLivrosAsync();
-    Task<bool> CriarLivroAsync(Livro livro);
-}
